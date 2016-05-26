70a2c81b351e40fdbb2d3260b4fb48504d209d0d
david-garcia-martin <deivi_gar@hotmail.com>
2016-05-26 13:25:44 +0200

    Tema 8: Bases de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,23 +1,40 @@
-//GET /question
+var models = require('../models');
+
+// GET /question
 exports.question = function(req, res, next) {
-	var answer = req.query.answer || '';
-	res.render('quizzes/question', {question: 'Capital de Italia',
-					answer: answer});
+	models.Quiz.findOne() // Busca la primera pregunta existente
+		.then(function(quiz) {
+			if (quiz) {
+				var answer = req.query.answer || '';
+
+				res.render('quizzes/question', {question: quiz.question,
+								                answer: answer});
+		    } else {
+		    	throw new Error('No hay preguntas en la BBDD.');
+		    }
+		})
+		.catch(function(error) {
+			next(error);
+		});
 };
 
-//GEET /check
+
+// GET /check
 exports.check = function(req, res, next) {
+	models.Quiz.findOne() // Busca la primera pregunta existente
+		.then(function(quiz) {
+			if (quiz) {
+				var answer = req.query.answer || "";
 
-	var answer = req.query.answer || "";
+				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
 
-	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result,
-					answer: answer});
-};
-
-exports.author = function(req, res, next){
-	res.render('quizzes/author');
-
-
-
+				res.render('quizzes/result', { result: result, 
+											   answer: answer });
+		    } else {
+		    	throw new Error('No hay preguntas en la BBDD.');
+		    }
+		})
+		.catch(function(error) {
+			next(error);
+		});
 };
